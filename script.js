73d59075304fcea86cc82b06ea46ba1a9b1c673e
Daryn01 <musaevdaryn1245@gmail.com>
2022-04-19 09:12:21 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let h1 = document.getElementById('h1')
+let text1 = document.getElementById('text1')
+let text2 = document.getElementById('text2')
+let text3 = document.getElementById('text3')
+
+h1.style.fontSize='40px'
+text1.style.color='blue'
+text2.style.color='grey'
+text3.style.color='red'
+
+
+
+
+let text4 = document.getElementById('text2')
+function dake0(){
+    text4.style.backgroundColor = 'green'
+}
+
+
+
+
+let input1 = document.getElementById('input1')
+function dake(){
+    input1.style.color = 'orange'
+}
+
+
+
+let input2 = document.getElementById('input2')
+function dake1(){
+    input2.value = 'Aquaman';
+}
+
+
+function dake2(){
+
+let input3 = document.getElementById('input3')
+let input4 = document.getElementById('input4')
+
+
+
+
+    
+input4.value = input3.value
+input3.value = "Mister Bean"
+
+
+}
+
+
+
